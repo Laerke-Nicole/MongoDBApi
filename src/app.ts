40b6dd757b1bb47b813c0fb4e40e0995908f75e0
Lærke Nielsen <laenie01@easv365.dk>
2025-02-16 14:42:04 +0100

    transpiling

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 import dotenvFlow from 'dotenv-flow';
+import routes from './routes';
 
-dotenvFlow.config();
+//dotenvFlow.config();
 
 // create express application
 const app: Application = express();
+
+app.use('/api', routes);
 
 export function startServer() {
     app.listen(4000, function() {
