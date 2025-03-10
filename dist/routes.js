c60dd54168e4e7ea4e9221fea9e31c5460e71b3b
Lærke Nielsen <laenie01@easv365.dk>
2025-03-10 10:38:04 +0100

    faker added to seeder

--- dist/routes.js
+++ dist/routes.js
@@ -115,12 +115,6 @@ router.post('/books', authController_1.verifyToken, bookController_1.createBook)
  *       - Book Routes
  *     summary: Get all books
  *     description: Get all books
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Book'
  *     responses:
  *       201:
  *         description: Got all books
@@ -143,25 +137,21 @@ router.get('/books', bookController_1.getAllBooks);
  *     tags:
  *       - Book Routes
  *     summary: Get book by ID
- *     description: Get book by ID
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Book'
+ *     description: Got book by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the book
+ *         schema:
+ *           type: string
  *     responses:
  *       201:
- *         description: Got book by ID
+ *         description: Successfully got the book
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                 _id:
- *                   type: string
+ *               $ref: '#/components/schemas/Book'
  */
 router.get('/books/:id', bookController_1.getBookByID);
 // update
@@ -270,12 +260,6 @@ router.post('/reviews', authController_1.verifyToken, reviewController_1.createR
  *       - Review Routes
  *     summary: Get all reviews
  *     description: Get all reviews
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Book'
  *     responses:
  *       201:
  *         description: Got all reviews
@@ -298,25 +282,21 @@ router.get('/reviews', reviewController_1.getAllReviews);
  *     tags:
  *       - Review Routes
  *     summary: Get review by ID
- *     description: Get review by ID
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Review'
+ *     description: Got review by ID
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: ID of the review
+ *         schema:
+ *           type: string
  *     responses:
  *       201:
- *         description: Got review by ID
+ *         description: Successfully got the review
  *         content:
  *           application/json:
  *             schema:
- *               type: object
- *               properties:
- *                 error:
- *                   type: string
- *                 _id:
- *                   type: string
+ *               $ref: '#/components/schemas/Review'
  */
 router.get('/reviews/:id', reviewController_1.getReviewByID);
 // update
--- src/util/dataSeeder.ts
+++ src/util/dataSeeder.ts
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt";
 import dotenvFlow from "dotenv-flow";
+import { faker } from '@faker-js/faker';
 
 // imports
 import { bookModel } from "../models/bookModel";
@@ -44,142 +45,49 @@ export async function seedData() {
 
     // seed for users
     const user1 = new userModel();
-    user1.name = "Jane Doe";
-    user1.email = "janedoe@gmail.com";
+    user1.name = faker.person.fullName();
+    user1.email = faker.internet.email();
     user1.password = passwordHash;
     await user1.save();
 
     const user2 = new userModel();
-    user2.name = "John Doe";
-    user2.email = "johndoe@gmail.com";
+    user2.name = faker.person.fullName();
+    user2.email = faker.internet.email();
     user2.password = passwordHash;
     await user2.save();
 
-    // seed for books  
-    const books = [
-        {
-            title: "A court of thorns and roses",
-            author: "Sarah J Maas",
-            description: "When nineteen-year-old huntress Feyre kills a wolf in the woods, a terrifying creature arrives to demand retribution. Dragged to a treacherous magical land she knows about only from legends, Feyre discovers that her captor is not truly a beast, but one of the lethal, immortal faeries who once ruled her world.",
-            genre: "Fantasy",
-            imageURL: "https://picsum.photos/500/500",
-            releaseYear: 2015,
-            price: 115,
-            stock: 100,
-            discount: true,
-            discountPct: 10,
-            isHidden: false,
-            _createdBy: user2.id,
-        },
-        {
-            title: "Fourth wing",
-            author: "Rebecca Yarros",
-            description: "Twenty-year-old Violet Sorrengail was supposed to enter the Scribe Quadrant, living a quiet life among books and history. Now, the commanding general—also known as her tough-as-talons mother—has ordered Violet to join the hundreds of candidates striving to become the elite of Navarre: dragon riders.",
-            genre: "Fantasy",
-            imageURL: "https://picsum.photos/500/500",
-            releaseYear: 2023,
-            price: 169,
-            stock: 20,
-            discount: true,
-            discountPct: 10,
-            isHidden: false,
-            _createdBy: user1.id,
-        },
-        {
-            title: "Iron flame",
-            author: "Rebecca Yarros",
-            description: "Everyone expected Violet Sorrengail to die during her first year at Basgiath War College—Violet included. But Threshing was only the first impossible test meant to weed out the weak-willed, the unworthy, and the unlucky.",
-            genre: "Fantasy",
-            imageURL: "https://picsum.photos/500/500",
-            releaseYear: 2023,
-            price: 169,
-            stock: 30,
-            discount: false,
-            discountPct: 0,
-            isHidden: false,
-            _createdBy: user1.id,
-        },
-        {
-            title: "Onyx Storm",
-            author: "Rebecca Yarros",
-            description: "After nearly eighteen months at Basgiath War College, Violet Sorrengail knows there's no more time for lessons. No more time for uncertainty. Because the battle has truly begun, and with enemies closing in from outside their walls and within their ranks, it's impossible to know who to trust.",
-            genre: "Fantasy",
-            imageURL: "https://picsum.photos/500/500",
-            releaseYear: 2025,
-            price: 200,
-            stock: 200,
-            discount: false,
-            discountPct: 0,
-            isHidden: false,
-            _createdBy: user1.id,
-        },
-        {
-            title: "The housemaid",
-            author: "Freidda McFadden",
-            description: "Welcome to the family, Nina Winchester says as I shake her elegant, manicured hand. I smile politely, gazing around the marble hallway. Working here is my last chance to start fresh. I can pretend to be whoever I like. But I'll soon learn that the Winchesters' secrets are far more dangerous than my own…",
-            genre: "Thriller",
-            imageURL: "https://picsum.photos/500/500",
-            releaseYear: 2022,
-            price: 115,
-            stock: 70,
-            discount: false,
-            discountPct: 0,
-            isHidden: false,
-            _createdBy: user1.id,
-        }, 
-    ];
+    // seed for books 
+    const books = [];
+    for (let i = 0; i < 10; i++) {
+        books.push({
+        title: faker.lorem.words(),
+        author: faker.person.fullName(),
+        description: faker.lorem.words(),
+        genre: faker.lorem.word(),
+        imageURL: faker.image.url(),
+        releaseYear: faker.date.past({ years: 50 }).getFullYear(),
+        price: faker.number.int({ min: 1, max: 5000 }),
+        stock: faker.number.int({ min: 0, max: 500 }),
+        discount: true,
+        discountPct: faker.number.int(),
+        isHidden: false,
+        _createdBy: user2.id,
+        });
+    }
 
     // save book _id from MongoDB
     const savedBooks = await bookModel.insertMany(books) 
 
     // seed for reviews  
-    const reviews = [
-        {
-            _book: savedBooks[0]._id,
-            _createdBy: user1.id,
-            rating: 4,
-            comment: "Great book",
-        }
-        ,
-        {
-            _book: savedBooks[2]._id,
-            _createdBy: user1.id,
-            rating: 7,
-            comment: "It was really good",
-        },
-        {
-            _book: savedBooks[3]._id,
-            _createdBy: user1.id,
-            rating: 10,
-            comment: "Even better than the first book",
-        },
-        {
-            _book: savedBooks[4]._id,
-            _createdBy: user1.id,
-            rating: 7,
-            comment: "Great book",
-        },
-        {
-            _book: savedBooks[4]._id,
-            _createdBy: user2.id,
-            rating: 6,
-            comment: "Recommend it",
-        },
-        {
-            _book: savedBooks[3]._id,
-            _createdBy: user2.id,
-            rating: 8,
-            comment: "Was better than expected",
-        },
-        {
-            _book: savedBooks[1]._id,
-            _createdBy: user2.id,
-            rating: 8,
-            comment: "Loved it",
-        }
-        
-        
-    ];
+    const reviews = [];
+    for (let i = 0; i < 10; i++) {
+        reviews.push({
+        _book: savedBooks[0]._id,
+        _createdBy: user1.id, 
+        rating: faker.number.int({ min: 1, max: 10 }),
+        comment: faker.lorem.words(),
+        });
+    }
 
     await reviewModel.insertMany(reviews);
 
