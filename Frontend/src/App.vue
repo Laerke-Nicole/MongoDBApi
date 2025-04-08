3f9838b4bf414edc4ef332194a571a3f02343105
Lærke Nielsen <laenie01@easv365.dk>
2025-04-08 20:19:33 +0200

    fetched the book data

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,47 +1,30 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, onMounted } from 'vue';
+import type { Book } from '../../src/interfaces/book'
+
+const books = ref<Book[]>([]);
+
+onMounted(async () => {
+  try {
+    const response = await fetch('https://mongodbapi-w61d.onrender.com/api/books');
+    const data = await response.json();
+    books.value = data;
+  } catch (error) {
+    console.error('Failed to fetch books:', error);
+  }
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
   <main>
-    <TheWelcome />
+    <div v-for="book in books" :key="book.id" class="book-card">
+      <img :src="book.imageURL" :alt="book.title" />
+      <h4>{{ book.title }}</h4>
+      <p>{{ book.author }}</p>
+    </div>
   </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
